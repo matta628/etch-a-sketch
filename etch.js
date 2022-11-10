f6c19afb8c5c7806df7eed304c17e591f4ec00c8
Matt <matta628@gmail.com>
2022-11-10 13:40:41 -0500

    Add slider and text number input to change dims

--- etch.js
+++ etch.js
@@ -4,26 +4,54 @@ container.style.flexDirection = "column";
 container.style.border = "50px solid goldenrod";
 container.style.borderRadius = "50px";
 
-let totalLen = 500;
-let n = 16;
-for (let i = 0; i < n; i++){
-    const rowDiv = document.createElement('div');
-    rowDiv.style.display = "flex";
-    for (let j = 0; j < n; j++){
-        const element = document.createElement('div');
-        element.classList.add("cell");
-        element.style.width = `${totalLen/n}px`;
-        element.style.height = `${totalLen/n}px`;
-        element.style.backgroundColor = "#FAF9F6";
-        element.style.border = ".5px solid black";
-        rowDiv.appendChild(element);
+function clearGrid(parent){
+    while (parent.lastElementChild){
+        console.log(parent.lastElementChild);
+        parent.removeChild(parent.lastElementChild);
     }
-    container.appendChild(rowDiv);
 }
 
-const cells = document.querySelectorAll('.cell');
-cells.forEach(cell => {
+let totalLen = 500;
+let n = 16;
+function drawGrid(n){
+    clearGrid(container);
+    for (let i = 0; i < n; i++){
+        const rowDiv = document.createElement('div');
+        rowDiv.style.display = "flex";
+        for (let j = 0; j < n; j++){
+            const element = document.createElement('div');
+            element.classList.add("cell");
+            element.style.width = `${totalLen/n}px`;
+            element.style.height = `${totalLen/n}px`;
+            element.style.backgroundColor = "#FAF9F6";
+            element.style.border = ".5px solid black";
+            rowDiv.appendChild(element);
+        }
+        container.appendChild(rowDiv);
+    }
+
+    const cells = document.querySelectorAll('.cell');
+    cells.forEach(cell => {
     cell.addEventListener('mouseover', () => {
         cell.style.backgroundColor = 'gray';
-    })
+    });
 })
+
+}
+drawGrid(n);
+
+
+
+const slider = document.querySelector('#slider');
+slider.onchange = function () {
+    n = this.value;
+    drawGrid(n);
+    this.nextElementSibling.value = this.value;
+}
+
+const dimension = document.querySelector('#text-input');
+dimension.onchange = function () {
+    n = this.value;
+    drawGrid(n);
+    this.previousElementSibling.value = this.value;
+}
