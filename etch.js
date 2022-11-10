a1e1c280c4dd5856064203858849f4a29585b0be
Matt <matta628@gmail.com>
2022-11-10 12:53:22 -0500

    Refactor grid creation to allow diff grid size

--- etch.js
+++ etch.js
@@ -4,16 +4,26 @@ container.style.flexDirection = "column";
 container.style.border = "50px solid goldenrod";
 container.style.borderRadius = "50px";
 
-for (let i = 0; i < 16; i++){
+let totalLen = 500;
+let n = 16;
+for (let i = 0; i < n; i++){
     const rowDiv = document.createElement('div');
     rowDiv.style.display = "flex";
-    for (let j = 0; j < 16; j++){
+    for (let j = 0; j < n; j++){
         const element = document.createElement('div');
+        element.classList.add("cell");
+        element.style.width = `${totalLen/n}px`;
+        element.style.height = `${totalLen/n}px`;
         element.style.backgroundColor = "#FAF9F6";
-        element.style.width = "30px";
-        element.style.height = "30px";
         element.style.border = ".5px solid black";
         rowDiv.appendChild(element);
     }
     container.appendChild(rowDiv);
 }
+
+const cells = document.querySelectorAll('.cell');
+cells.forEach(cell => {
+    cell.addEventListener('mouseover', () => {
+        cell.style.backgroundColor = 'gray';
+    })
+})
